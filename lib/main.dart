0890e5929ddea4cd17dc67180525851269da4aa4
Alex Avila <alex.ramon2004@gmail.com>
2024-11-05 22:37:36 -0500

    feat: added presentation stuff.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'feature_about_us/presentation/home/about_us_screen.dart';
+import 'package:flutter_project/core/presentation/temporaly_theme.dart';
+import 'package:flutter_project/feature_about_us/presentation/home/about_us_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: AboutUsScreen(),
+    return const TemporalyTheme(
+      child: MaterialApp(
+        home: AboutUsScreen()
+      ),
     );
   }
 }
